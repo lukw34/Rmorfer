657a623b01294e6660122f2e896717c1832f7979
lukw34 <lukw34@gmail.com>
2019-04-18 15:12:52 +0200

    Add file handler and example

--- R/morfer.R
+++ R/morfer.R
@@ -1,19 +1,28 @@
-
 #' Retrieve a Java instance of Morfer
-#'
+#' @param filepath path which point to text file
 #' @return a Java instance of Morfer
 #' @export
-get_morfer<-function() {
+#'
+getMorfer <- function() {
   .jaddClassPath('inst/java/Morfer-lib-1.0-SNAPSHOT-jar-with-dependencies.jar')
   return (.jnew('uek/nlp/Morfer'))
 }
 
+#' Retrieve  a Java File object for specific path
+#' @param filepath path which point to text file
+#' @return an File object wich represent specific file
+#' @export
+
+getFileData <- function(filepath) {
+  return(.jnew('java/io/File', filepath))
+}
+
 #' Retrieve  array which represent plain data return by morfeusz library
-#' @param text_to_analyze Text to analyze
+#' @param text_to_analyze data
 #' @return an array in which every element represent one line from morfeusz result
 #' @export
-analyze_to_array <- function(text_to_analyze) {
-  morfer = get_morfer();
-  result_array <- .jcall(morfer, "[S", "analyzeToArray", text_to_analyze)
+analyzeToArray <- function(data) {
+  morfer = getMorfer();
+  result_array <- .jcall(morfer, "[S", "analyzeToArray", data)
   print(result_array)
 }
---
+++ example/example-usage.R
@@ -0,0 +1,4 @@
+install.packages('devtools')
+library(devtools)
+install_github('lukw34/Rmorfer')
+analyzeToArray(getFileData("./example/data/Harry Potter i Wiezien Azkabanu.txt"))
