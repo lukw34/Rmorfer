1b64d22bd2a0db2b8bdae1bad649907066c6888e
lukw34 <lukw34@gmail.com>
2019-05-26 11:34:59 +0200

    Update example

--- example/example-usage.R
+++ example/example-usage.R
@@ -2,10 +2,10 @@ library(stringr)
 
 install.packages('devtools')
 library(devtools)
-devtools::install_github('lukw34/Rmorfer', ref = "v0.1.0", force = TRUE)
+devtools::install_github('lukw34/Rmorfer', force = TRUE)
 
 library('Rmorfer')
-morfer <- getMorfer(paste(.libPaths()[1], "/libs/java", sep = ""));
+morfer <- getMorfer(paste(.libPaths()[1], "/Rmorfer/java/libs", sep = ""));
 arrayFromFile <- analyzeToArray(morfer, getFileData("./example/data/Harry Potter i Wiezien Azkabanu.txt"))
 arrayFromString <- analyzeToArray(morfer, "Ala ma bardzo dużo kotów")
 CSVFromFile <- analyzeToCSV(morfer, getFileData("./example/data/Harry Potter i Wiezien Azkabanu.txt"))
